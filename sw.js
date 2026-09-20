5d421a7d62fedabaf675fc652e01e837229ad1c1
jdaniel318-ctrl <jdaniel318@gmail.com>
2026-09-20 13:21:14 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const RELEASE_BUILD='8.8.15.7';
-const RELEASE_SEAL='owner-keel-88157';
+const RELEASE_BUILD='8.8.15.8';
+const RELEASE_SEAL='release-helm-88158';
 
 self.addEventListener('install', event => {
   // Atomic Seal: service worker is an identity/control-plane sentinel only.
